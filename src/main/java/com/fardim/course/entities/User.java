b4a3ece45719a7397cc7b308c4d21687c194f893
Enock <enocksm@outlook.com>
2023-08-08 13:49:52 -0300

    Atts em busca por usuarios, por id e armazenamento

--- src/main/java/com/fardim/course/entities/User.java
+++ src/main/java/com/fardim/course/entities/User.java
@@ -1,11 +1,14 @@
 package com.fardim.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,6 +24,11 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy  = "client")
+	private List<Order> orders = new ArrayList<>();
+	
+	
 	
 	public User() {
 		
@@ -77,7 +85,11 @@ public class User implements Serializable {
 		this.password = password;
 	}
 
-
+	public List<Order> getOrders() {
+		return orders;
+	}
+	
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -103,5 +115,6 @@ public class User implements Serializable {
 			return false;
 		return true;
 	}
+
 		
 }
