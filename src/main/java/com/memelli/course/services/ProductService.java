e5194488006b5b19b9f5cc053fea489b154e25f9
Enock <enocksm@outlook.com>
2023-08-09 16:27:00 -0300

    Adding Class Product

---
+++ src/main/java/com/memelli/course/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.memelli.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.memelli.course.entities.Product;
+import com.memelli.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired	
+	private ProductRepository repository;
+		
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
