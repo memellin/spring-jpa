e7c7dce616e0a314f1e256a3a5b5cf49eb7e6581
Enock <enocksm@outlook.com>
2023-08-11 18:27:49 -0300

    Adding subtotal and total of orders

--- src/main/java/com/memelli/course/entities/Order.java
+++ src/main/java/com/memelli/course/entities/Order.java
@@ -103,7 +103,15 @@ public class Order implements Serializable {
 	public Set<OrderItem> getItems(){
 		return items;
 	}
-
+	
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrderItem x : items) {
+			sum += x.getsubTotal();
+		}
+		return sum;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
